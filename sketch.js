b347c209f5a4ff9ebc7c727cbef323346b648615
yoyo2288 <56063952+yoyo2288@users.noreply.github.com>
2020-08-13 19:49:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,29 @@
-let array = [[1,2,3,4],
-             [5,6,7,8],
-             [9,10,11,12],
-             [13,14,15,16]];
+var starting_state = [[1, 2, 3, 4],
+                      [5, 6, 7, 8],
+                      [9, 10, 11, 12],
+                      [13, 14, 15, 16]];
+
+let moves = [];
+let state;
+
 
 function setup() {
-    createCanvas(400, 400);
-    background(220);noStroke();textSize(20);rectMode(CENTER);
+    createCanvas(600, 600);
+    background(220);noStroke();textSize(36);rectMode(CENTER);
+    final_position();
     
-    draw_array(array);
+    starting_node = new node(starting_state, []);
 
+    let ans = solve_15(starting_node);
+    moves = ans.steps;
+    state = starting_state;
+    frameRate(10);
   }
   
   function draw() {
-    
+    if(moves.length != 0){
+      state = next_state(moves[0],state);
+      moves.shift();
+      draw_array(state);
+    }
   }
