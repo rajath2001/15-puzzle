be07a24ab33562b5d8aa4ed49058a89ae6a49aaf
yoyo2288 <56063952+yoyo2288@users.noreply.github.com>
2020-08-13 19:50:30 +0530

    Update solve_15.js

--- solve_15.js
+++ solve_15.js
@@ -1,23 +1,28 @@
 function solve_15(starting_node){
+  
     let visited = new Set();
-    let current = [];
+    let current = new PriorityQueue();
    
-   current.push(starting_node);
+   current.push(starting_node,0);
 
-   while(current.length != 0){
-       let current_node = current[0];
+   while(current.size() < 100000){
+       let current_node = current.pop();
+      
        if(current_node.reached())
             return current_node;
-       visited.add([current_node.state()]);
-       current.shift();
+        
+        visited.add(convert_to_string(current_node.position));
+  
        var next_moves  = current_node.next();
+       var heuristic_current = heuristic(current_node.position);
 
        for(var i = 0; i < next_moves.length; i++ ){
-            if(!visited.has(next_moves[i].state())){
-                current.push(next_moves[i]);
-            }
+             if(!visited.has(convert_to_string(next_moves[i].position))){
+                current.push(next_moves[i],next_moves[i].steps.length + heuristic(next_moves[i].position) - heuristic_current);
+             }
        }
-
    }
+   console.log(current.pop());
+   return current.pop();
 }
 
